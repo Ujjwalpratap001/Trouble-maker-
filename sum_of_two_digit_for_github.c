72d7c5e2472feb962d48c6b4d71e8a9fd58aadf4
Ujjwal pratap <ujjwalpratapsingh624604@gmail.com>
2025-11-20 10:07:22 +0530

    Add files via upload

---
+++ sum_of_two_digit_for_github.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter First numbers: ");
+    scanf("%d", &a);
+    printf("Enter Second numbers: ");
+    scanf("%d",&b);
+    printf("The Sum Of Both Number is = %d", a+b);
+    return 0;
+}
